f66e18fa4c1fb523a5a37a497d89b2c65593b204
pemrouz <p3pedram@hotmail.com>
2015-08-09 22:50:36 +0100

    update to support dynamic __dirname

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var through = require('through2')
   , file = require('utilise.file')
   , resolve = require('path').resolve
+  , dirname = require('path').dirname
 
-module.exports = function () {
+module.exports = function (path) {
   return through(function (buf, enc, next) {
     this.push(
       buf
         .toString('utf8')
         .replace(/file\((.*?)\)/g, function($0, $1){ 
-          return '"' + file(resolve($1.replace(/'/g, ''))).replace(/\n/g, '\\n').replace(/\r/g, '') + '"'
+          var resolved = eval($1
+            .replace('__dirname', '"' + dirname(path).replace(/\\/g, '\\\\') + '"'))
+          
+          return '"' + file(resolve(resolved)).replace(/\n/g, '\\n').replace(/\r/g, '') + '"'
         }) 
     )
     next()
